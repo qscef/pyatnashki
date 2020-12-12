a562005b0bfbd146640266af0dcb83801d6a74dd
Andrew <lord_smert666@mail.ru>
2020-12-12 21:18:47 +0300

    верстка пк

---
+++ components/gameTile.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="game-tile" :class="{'empty': name === 0}">
+    {{ name }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {type: Number, default: 0},
+  }
+}
+</script>
+
+<style lang="scss">
+  @import '../assets/scss/styles.scss';
+  .game-tile {
+    background: $background-tile-color;
+    border-radius: 5px;
+    height: 85px;
+    width: 85px;
+    line-height: 80px;
+    font-size: 32px;
+    color: $text-main-color;
+    text-align: center;
+    cursor: pointer;
+
+    &.empty {
+      opacity: 0;
+    }
+  }
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,96 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        pyatnashki
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <div class="main">
+    <div class="header">
+      Пятнашки
+    </div>
+    <div class="menu">
+      <div class="menu__element">Время
+        <div>{{time}}</div>
       </div>
+      <div class="menu__element menu__element_restart" @click="restart()">Рестарт</div>
+      <div class="menu__element">Ходов
+        <div>{{step}}</div>
+      </div>   
+    </div>
+    <div class="game-field">
+        <game-tile :name="i" v-for="i in listTile" :key="i"></game-tile>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import gameTile from '../components/gameTile'
+
+export default {
+  components: { gameTile },
+  data() {
+    return {
+      time: 0,
+      step: 0,
+      listTile: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,0],
+    }
+  },
+  created() {
+    this.createBoard();
+  },
+  methods: {
+    restart() {
+      this.time= 0;
+      this.step = 0;
+      this.createBoard();
+    },
+    createBoard() {
+      this.listTile = this.listTile.sort(() => Math.random() - 0.5);
+    }
+  }
+}
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="scss">
+  @import '../assets/scss/styles.scss';
+  .main {
+    margin: 0 auto;
+    width: 400px;
+    background: $background-color;
+    border-radius: 10px;
 
-.title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
+    .header {
+      text-align: center;
+      font-size: 32px;
+      color: $text-main-color;
+      padding-top: 20px;
+      margin-bottom: 20px;
+    }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
+    .menu {
+      display: flex;
+      justify-content: space-around;
+      margin-bottom: 10px;
 
-.links {
-  padding-top: 15px;
-}
+      .menu__element {
+        width: 100px;
+        text-align: center;
+        line-height: 20px;
+        border-radius: 10px;
+        background: $background-field-color;
+        cursor: pointer;
+
+        &.menu__element_restart {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+        }
+      }
+    }
+
+    .game-field {
+      margin: 5px;
+      padding: 10px;
+      border-radius: 5px;
+      background: $background-field-color;
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr 1fr;
+      grid-gap: 5px;
+    }
+  }
 </style>
